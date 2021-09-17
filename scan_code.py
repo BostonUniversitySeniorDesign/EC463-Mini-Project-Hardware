3ed9e8c3cf4442e806af6c3e132a6c7102470630
Jatme26 <72499097+Jatme26@users.noreply.github.com>
2021-09-17 14:48:18 -0400

    Update scan_code.py

--- scan_code.py
+++ scan_code.py
@@ -2,8 +2,8 @@ import os
 import time
 
 while True: 
-        os.system('sudo python3 wifi_scan.py ./wifi_data -N 1');
-        os.system('sudo python3 ble_scan.py >> ble_data.txt');
+        os.system('sudo python3 wifi_scan.py ./wifi_data -N 1');  #Just one loop in sudo command because we will be looping every 30 seconds
+        os.system('sudo python3 ble_scan.py >> ble_data.txt'); #Add all the data at the end of the ble_data.txt file for every iteration
 time.sleep(30);
 
 
